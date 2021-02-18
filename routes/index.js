86339501da89ef2cb91007ac66acad91b04d1af2
tjuacalla123 <ftjuacal@ucsd.edu>
2021-02-18 10:48:52 -0800

    .

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,11 @@ var projects = require('../projects.json');
  */
 
 exports.view = function(request, response){
+	data['viewAlt'] = false;
   	response.render('index', projects);
 };
+
+exports.viewAlt = function(request, response) {
+	data['viewAlt'] = true;
+	response.render('index',projects);
+}
