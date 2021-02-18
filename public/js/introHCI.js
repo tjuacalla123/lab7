dfd4a97ab7df6e41258a887c1bcd2f7edad43b29
tjuacalla123 <ftjuacal@ucsd.edu>
2021-02-18 11:09:42 -0800

    done

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,12 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
+	$(".likeBtn").click(clickLikeBtn);
 	// your code here
+}
+
+function clickLikeBtn() {
+	e.preventDefault();
+	ga('create', 'UA-190116327-1', 'auto');
+	ga('send', 'event', 'signup', 'click');
 }
